0f7ccecc7b03dfb9ce0fba85278d4916536ab139
Jack Xu <jack.xu@aig.com>
2018-03-07 13:47:56 -0500

    some modification to the logic

--- spec/neural_network_spec.rb
+++ spec/neural_network_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe TensorflowRb::NeuralNetwork do
 
-  describe '::train', focus: true do
+  describe '::train' do
     let(:neuron_count)  {3}
     let(:learning_rate) {0.05}
     let(:x) { Numo::DFloat[[0,0], [0,1], [1,0], [1,1]] }
@@ -16,23 +16,23 @@ RSpec.describe TensorflowRb::NeuralNetwork do
       it 'reduces the error for each step' do 
         error1, error2 = 0, 0
         network.train() {|n| error1 = TensorflowRb.l2error(n.target, n.output)}
+        100.times { network.train() }
         network.train() {|n| error2 = TensorflowRb.l2error(n.target, n.output)}
   
         expect(error1).to be > error2
       end
-        
     end
 
-    context 'train for 40000 epochs' do
-      let(:learning_rate) {0.03}
-      let(:epochs) { 40000 }
+    context 'train for 100000 epochs' do
+      let(:learning_rate) {0.05}
+      let(:epochs) { 100000 }
       before do        
         network.input = x
         network.target = y
       end
 
       it 'reduces the error for each step' do 
-        epochs.times {network.train() {|x| puts TensorflowRb.l2error(network.target, network.output) if network.epoch%1000 == 0  }}
+        epochs.times {network.train() {|x|  TensorflowRb.l2error(network.target, network.output) if network.epoch%1000 == 0  }}
         expect(TensorflowRb.l2error(network.target, network.output)).to be < 0.65
       end 
     end    
