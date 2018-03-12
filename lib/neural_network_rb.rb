63ea7092527ee96cf511aa9f11d31ad62555009b
Jack Xu <jack.xu@aig.com>
2018-03-12 10:18:30 -0400

    change the softmax function

--- lib/neural_network_rb.rb
+++ lib/neural_network_rb.rb
@@ -5,7 +5,6 @@ require 'neural_network_rb/narray'
 require 'neural_network_rb/mnist_image'
 require 'neural_network_rb/mnist'
 require "neural_network_rb/version"
-require 'neural_network_rb/softmax'
-require 'neural_network_rb/sigmoid'
+require 'neural_network_rb/activations'
 require 'neural_network_rb/error'
 require 'neural_network_rb/neural_network'
--- spec/sigmoid_spec.rb
+++ spec/sigmoid_spec.rb
@@ -5,24 +5,24 @@ RSpec.describe NeuralNetworkRb do
     let(:result) { NeuralNetworkRb.sigmoid(x) }
 
     context 'negative infinity' do
-      let(:x) { Numo::DFloat[-10000] }
+      let(:x) { Numo::DFloat[[-10000, 0], [-10000, 0]] }
       it 'outputs close to 0' do
-        expect(result[0]).to be < 0.000001
+        expect(result.to_a).to eql([[0.0, 0.5], [0.0, 0.5]])
       end
     end
 
-
     context 'positive infinity' do
-      let(:x) { Numo::DFloat[10000] }
+      let(:x) { Numo::DFloat[[10000, 0], [10000, 0]] }
       it 'outputs close to 1' do
-        expect(result[0]).to be > 0.99999999
+        p result
+        expect(result.to_a).to eql([[0.5, 0.0], [0.5, 0.0]])
       end
     end
 
     context 'zero' do
-      let(:x) { Numo::DFloat[0] }
-      it 'outputs 0.5' do
-        expect(result[0]).to be(0.5) 
+      let(:x) { Numo::DFloat[[0], [0]] }
+      it 'outputs 0.5'  do
+        expect(result.to_a).to eql([[0.5], [0.5]]) 
       end
     end
   end
@@ -32,23 +32,23 @@ RSpec.describe NeuralNetworkRb do
     let(:result) { NeuralNetworkRb.sigmoid_prime(x) }
 
     context 'negative infinity' do
-      let(:x) { Numo::DFloat[-10000] }
+      let(:x) { Numo::DFloat[[-10000, 0], [-10000, 0]] }
       it 'outputs close to 0' do
-        expect(result[0]).to be < 0.000001
+        expect(result.to_a).to eq([[0, 0.25], [0, 0.25]])
       end
     end
 
     context 'positive infinity' do
-      let(:x) { Numo::DFloat[10000] }
+      let(:x) { Numo::DFloat[[10000, 0], [10000, 0]] }
       it 'outputs close to 0' do
-        expect(result[0]).to be < 0.000001
+        expect(result.to_a).to eq([[0.25, 0], [0.25, 0]])
       end
     end
 
     context 'zero' do
-      let(:x) { Numo::DFloat[0] }
+      let(:x) { Numo::DFloat[[0], [0]] }
       it 'calculate sigmoid output' do
-        expect(result[0]).to be(0.25) 
+        expect(result.to_a).to eq([[0.25], [0.25]])
       end
     end
   end
